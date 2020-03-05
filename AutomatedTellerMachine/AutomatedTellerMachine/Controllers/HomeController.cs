5aa76f570400924dd6317d74dab652df59539b0a
olabisi olawale A <ejiolawale4@gmail.com>
2020-03-05 10:18:16 +0100

    balance enquiry and deposite fixed

---
+++ AutomatedTellerMachine/AutomatedTellerMachine/Controllers/HomeController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using AutomatedTellerMachine.Models;
+
+namespace AutomatedTellerMachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        [Authorize]
+        //GET /home/index
+        public ActionResult Index()
+        {
+            var userId = User.Identity.GetUserId();
+            var checkingAccountId = db.CheckingAccounts.Where(c => c.ApplicationUserId == userId).First().Id;
+            ViewBag.CheckingAccountId = checkingAccountId;
+            return View();
+        }
+
+        //GET /home/about
+        [ActionName("about-this-atm")]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View("About");
+        }
+        public ActionResult Contact()
+        {
+            ViewBag.TheMessage = "Having toruble? send us a mesage.";
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(string message)
+        {
+            //TODO: send a message to the HQ
+            ViewBag.TheMessage = "Thanks, we got your message. ";
+            return View();
+        }
+        public ActionResult Foo()
+        {
+            return View();
+        }
+
+        public ActionResult Serial(string lettercase)
+        {
+            var serial = "ASPNETMVCATM";
+            if(lettercase == "lower")
+            {
+                return Content(serial.ToLower());
+            }
+            //return new HttpStatusCodeResult(403);
+            // return Json(new { name = "serial", value = serial }, JsonRequestBehavior.AllowGet);
+            return RedirectToAction("Index");
+        }
+    }
+
+}
---
+++ AutomatedTellerMachine/AutomatedTellerMachine/Models/Transaction.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Providers.Entities;
+using Microsoft.AspNet.Identity;
+
+namespace AutomatedTellerMachine.Models
+{
+    public class Transaction
+    {
+        
+        public int Id { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        public decimal Amount { get; set; }
+
+        
+        [Required]
+        public int CheckingAccountId{ get; set; }
+        public virtual CheckingAccount CheckingAccount { get; set; }
+
+    }
+}
