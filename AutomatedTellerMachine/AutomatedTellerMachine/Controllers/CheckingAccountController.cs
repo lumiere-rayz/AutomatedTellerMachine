37eb9761fca1820925f56ffffb5e3129a7c91e87
olabisi olawale A <ejiolawale4@gmail.com>
2020-03-06 18:42:55 +0100

    Admin user added with details function implemented

--- AutomatedTellerMachine/AutomatedTellerMachine/Controllers/CheckingAccountController.cs
+++ AutomatedTellerMachine/AutomatedTellerMachine/Controllers/CheckingAccountController.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
 
 namespace AutomatedTellerMachine.Controllers
 {
+    [Authorize]
     public class CheckingAccountController : Controller
     {
+        private ApplicationDbContext db = new ApplicationDbContext(); 
         // GET: CheckingAccount
         public ActionResult Index()
         {
@@ -18,16 +21,32 @@ namespace AutomatedTellerMachine.Controllers
         // GET: CheckingAccount/Details
         public ActionResult Details()
         {
-            var checkingAccount = new CheckingAccount
-            {
-                AccountNumber = "0000123456",
-                FirstName = "Olawale",
-                LastName = "Olabisi",
-                Balance = 500
-            };
+            //to get the checking account from the db set, first grab thge userId which requires the microsoft.Aspnet.Identity namespace
+            var userId = User.Identity.GetUserId();
+            // filter on the actual checkingAccount db set with the where() and filter on ApplicationUserId and take the first result
+            var checkingAccount = db.CheckingAccounts.Where(c => c.ApplicationUserId == userId).First();
+            
             return View(checkingAccount);
         }
 
+        // GET: CheckingAccount/DetailsForAdmin
+        [Authorize(Roles ="Admin")] // only to be access by user with the admin role
+        public ActionResult DetailsForAdmin(int id)
+        {
+
+            
+            var checkingAccount = db.CheckingAccounts.Find(id);
+
+            return View("Details",checkingAccount);
+        }
+
+        //adding a method for the admin user so they can view the balance for any account by parsing the checkingAccount id
+        // to easy access the account we need a view that list all the existing Accounts
+        [Authorize(Roles = "Admin")] //only to be access by user with the admin role
+        public ActionResult List()
+        {
+            return View(db.CheckingAccounts.ToList());
+        }
         // GET: CheckingAccount/Create
         public ActionResult Create()
         {
--- AutomatedTellerMachine/AutomatedTellerMachine/Migrations/Configuration.cs
+++ AutomatedTellerMachine/AutomatedTellerMachine/Migrations/Configuration.cs
@@ -1,5 +1,9 @@
 namespace AutomatedTellerMachine.Migrations
 {
+    using AutomatedTellerMachine.Models;
+    using AutomatedTellerMachine.Services;
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -15,6 +19,29 @@ namespace AutomatedTellerMachine.Migrations
 
         protected override void Seed(AutomatedTellerMachine.Models.ApplicationDbContext context)
         {
+            //set up roles and asign users quickly
+            //create an instance of a user store
+            var userStore = new UserStore<ApplicationUser>(context);
+            // create a user manager by specifying the application user class and passing in the store we just created
+            var userManager = new UserManager<ApplicationUser>(userStore);
+
+            if(!context.Users.Any(t=>t.UserName == "admin@mvcatm.com"))
+            {
+                var user = new ApplicationUser { UserName = "admin@mvcatm.com", Email = "admin@mvcatm.com" };
+                userManager.Create(user, "passW0rd!");
+
+                //after creating the admin user. set it up with a checking account using checking account service
+                var service = new CheckingAccountService(context);
+                service.CreateCheckingAccount("admin", "user", user.Id, 1000);
+
+                //now to create the admin role and give it a new identity role called admin
+                //the lambda expression tell it to look for existing record using the name property
+                context.Roles.AddOrUpdate(r => r.Name, new IdentityRole { Name = "Admin" });
+                context.SaveChanges(); //to ensure the role exist in the database
+                //finally use the userManager AddToRole() to assign the new user to the admin role
+                userManager.AddToRole(user.Id, "Admin"); //done run Update-Database command and test
+            }
+
             //  This method will be called after migrating to the latest version.
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
